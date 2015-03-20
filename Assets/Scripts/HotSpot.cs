682428fa491137f4b513ee3e1e08781f6e53385e
jpparent <jeanp.parent@gmail.com>
2015-03-19 22:35:24 -0400

    add first draft of the hotspot script

---
+++ Assets/Scripts/HotSpot.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HotSpot : MonoBehaviour {
+
+    
+    private bool isHatInRange = false;
+
+    public bool isActive = false;
+    public float scoreIncreaseRate = 1f; // one second delay between score increment
+    public float nextScoreIncrease;
+    public int scoreValue = 100;    // staying inside the hotspot for [delayBetweenScoreIncrement] seconds increment the hat's score by this amount
+
+    public GameObject gameManager;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+    void OnTriggerStay2D( Collider2D other){
+
+        if( other.tag == "playerHat" && Time.time > nextScoreIncrease ){
+
+            nextScoreIncrease = Time.time + scoreIncreaseRate; 
+
+            // gameManager.playerHat ? .score += this.scoreValue
+
+        }
+    }
+
+
+}
