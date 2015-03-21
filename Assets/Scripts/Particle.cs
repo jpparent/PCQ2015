ec02c802d1b5910046e15a9c42aa74b4c5f3e42c
Cyberslash17 <gabrieltrahan@hotmail.com>
2015-03-20 21:32:59 -0400

    PARTICLES !!!!!!!!!

---
+++ Assets/Scripts/Particle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Particle : MonoBehaviour {
+
+    public bool invert;
+    public Transform Particle_pad; 
+	// Update is called once per frame
+	void Update () {
+        if (invert)
+        {
+            transform.position = new Vector3((-Mathf.Sin(Time.time) / 3) + Particle_pad.position.x, 0, (-Mathf.Cos(Time.time) / 3) + Particle_pad.position.z);
+        }
+        else {
+            transform.position = new Vector3((Mathf.Sin(Time.time) / 3) + Particle_pad.position.x, 0, (Mathf.Cos(Time.time) / 3) + Particle_pad.position.z);
+        }
+        
+        //this.GetComponent<ParticleSystem>().startSpeed = Mathf.Abs(Mathf.Sin(Time.time*10 +1))/5;
+
+        
+	}
+}
